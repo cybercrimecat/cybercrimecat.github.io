fd28a62554894fe4ff80dc423d59103281693e83
Minoa <57844837+M1noa@users.noreply.github.com>
2024-10-17 18:46:00 -0500

    Update index.js

--- index.js
+++ index.js
@@ -11,9 +11,6 @@ app.use('/unlisted', (req, res, next) => {
   res.status(403).send('Access to this directory is forbidden.');
 });
 
-// Serve static files from the 'site' folder
-app.use(express.static(siteDir));
-
 // Function to generate the HTML for the post list
 function generatePostListHtml(posts) {
   let postListHtml = '<ul>';
@@ -24,16 +21,17 @@ function generatePostListHtml(posts) {
   return postListHtml;
 }
 
-// Function to inject the post list into HTML files
-function injectPostList(html, postListHtml) {
-  return html.replace('<div id="recentpostlistdiv"></div>', `<div id="recentpostlistdiv">${postListHtml}</div>`)
-             .replace('<div id="postlistdiv"></div>', `<div id="postlistdiv">${postListHtml}</div>`);
+// Function to inject the post list into any file (HTML, CSS, JS, etc.)
+function injectPostList(content, postListHtml) {
+  return content.replace('<div id="recentpostlistdiv"></div>', `<div id="recentpostlistdiv">${postListHtml}</div>`)
+                .replace('<div id="postlistdiv"></div>', `<div id="postlistdiv">${postListHtml}</div>`);
 }
 
-// Serve all HTML files in the site directory, injecting the post list if needed
+// Serve all files in the site directory, injecting the post list if needed
 app.get('/*', (req, res) => {
   const filePath = path.join(siteDir, req.path);
 
+  // Scan the posts directory
   fs.readdir(postsDir, (err, files) => {
     if (err) {
       return res.status(500).send('Unable to scan posts directory');
@@ -48,15 +46,15 @@ app.get('/*', (req, res) => {
 
     const postListHtml = generatePostListHtml(posts);
 
-    // Read the requested HTML file and inject the post list if placeholders are found
-    fs.readFile(filePath, 'utf8', (err, html) => {
+    // Read the requested file (HTML, CSS, JS, etc.)
+    fs.readFile(filePath, 'utf8', (err, content) => {
       if (err) {
         return res.status(404).send('File not found');
       }
 
       // Inject post list if placeholders exist
-      const updatedHtml = injectPostList(html, postListHtml);
-      res.send(updatedHtml);
+      const updatedContent = injectPostList(content, postListHtml);
+      res.send(updatedContent);
     });
   });
 });
