0d0dae5bfcaf66bc0c4ad43faa297dde446e44bf
Minoa <57844837+M1noa@users.noreply.github.com>
2024-10-17 18:09:21 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,15 +3,16 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const postsDir = path.join(__dirname, 'site/posts'); // Adjust the path if necessary
+const siteDir = path.join(__dirname, 'site'); // Serve from the 'site' directory
+const postsDir = path.join(siteDir, 'posts'); // Adjusted path for posts
 
 // Middleware to block access to /unlisted/ path
 app.use('/unlisted', (req, res, next) => {
   res.status(403).send('Access to this directory is forbidden.');
 });
 
-// Serve static files from the public folder
-app.use(express.static('site'));
+// Serve static files from the 'site' folder
+app.use(express.static(siteDir)); // Now serving the entire 'site' folder
 
 // API to get the list of posts
 app.get('/api/posts', (req, res) => {
@@ -19,7 +20,7 @@ app.get('/api/posts', (req, res) => {
     if (err) {
       return res.status(500).json({ error: 'Unable to scan posts directory' });
     }
-    
+
     // Filter HTML files
     const posts = files
       .filter(file => file.endsWith('.html'))
@@ -27,13 +28,13 @@ app.get('/api/posts', (req, res) => {
         filename: file,
         title: file.replace(/-/g, ' ').replace('.html', ''),
       }));
-    
+
     res.json(posts);
   });
 });
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/site/index.html');
+  res.sendFile(path.join(siteDir, 'index.html')); // Serve index.html from 'site'
 });
 
 const PORT = process.env.PORT || 3000;
